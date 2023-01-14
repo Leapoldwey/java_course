4b0e9de085b4b419c5d32e670089eb13d2f88a4e
mihail <mihail_mihailow@list.ru>
2023-01-14 22:12:53 +0500

    lr7

--- src/lr7/Example1/Main.java
+++ src/lr7/Example1/Main.java
@@ -1,6 +1,22 @@
-package lr7;
+package lr7.Example1;
 
-public class Class7 {
+public class Main {
+    public static void main(String[] args) {
+
+        SuperClass superClass = new SuperClass("передал в конструктор суперкласса");
+        String className = superClass.toString();
+        System.out.println(className);
+
+        SubClass subClass = new SubClass("передал в конструктор подкласса");
+        String subClassName = subClass.toString();
+        System.out.println(subClassName);
+
+        SubClass subClass2 = new SubClass("передал в конструктор подкласса", "где два параметра");
+        String subClassName2 = subClass2.toString();
+        System.out.println(subClassName2);
+
+
+    }
 }
 /*
 1. Напишите программу, в которой есть суперкласс с приватным текстовым полем,
