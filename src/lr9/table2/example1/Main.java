5e871eec28ba61d41cf8b7649d2c3f25bffc0d7c
mihail <mihail_mihailow@list.ru>
2023-03-01 20:07:17 +0500

    lr9

---
+++ src/lr9/table2/example1/Main.java
@@ -0,0 +1,44 @@
+package lr9.table2.example1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        try {
+            System.out.print("Введите длину массива: ");
+            int arrLength = in.nextInt();
+
+            int[] arr = new int[arrLength];
+
+            double avg = 0.0;
+            int count = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = in.nextInt();
+                avg += arr[i];
+
+                if (arr[i] > 0) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                throw new ArithmeticException("положительные элементы отсутствуют");
+            }
+            avg /= arrLength;
+            System.out.print("Среднее значение: " + avg + ", список: " + Arrays.toString(arr));
+        } catch (InputMismatchException e) {
+            System.out.println("Несоответствие числового типа данных");
+        }  catch (ArithmeticException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
+/*
+В программе, вычисляющей среднее значение среди положительных элементов одномерного массива (тип элементов int),
+ вводимого с клавиатуры, могут возникать ошибки в следующих случаях:
+–	ввод строки вместо числа;
+–	несоответствие числового типа данных;
+–	положительные элементы отсутствуют.
+ */
